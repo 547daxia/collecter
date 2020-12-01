e47ddc95a2b7bfba563bb35e7e2482159823ec79
qt_fei <qt_fei@foxmail.com>
2020-12-01 11:58:37 +0800

    ...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,12 +16,35 @@ export default {
         { href: '', label: '1' },
         { href: '', label: '2' },
         { href: '', label: '3' }
-      ]
+      ],
+      keyWord: '诛仙'
     }
   },
   async mounted () {
-    const data = this.$axios.get('https://www.baidu.com')
-    console.log(data)
+    const data = await this.$axios.get(
+      `https://www.qidian.com/search?kw=${this.keyWord}`
+    )
+    const $ = this.$cheerio.load(data.data)
+    // const tmpArr = $(
+    //   '#result-list > .book-img-text > ul >li > .book-img-box > a'
+    // )
+    // const imageArr = $(
+    //   '#result-list > .book-img-text > ul >li > .book-img-box > a > img'
+    // )
+    const bookInfo = $(
+      '#result-list > .book-img-text > ul >li > .book-mid-info'
+    )
+    bookInfo[0].children.forEach(item => {
+      if (item.attribs) {
+        if (item.attribs.class) {
+          // console.log(item.attribs.class)
+          item.text()
+        }
+      }
+    })
+
+    // console.log('https:' + imageArr[0].attribs.src) // 封面
+    // console.log('https:' + tmpArr[0].attribs.href) // 链接
   }
 }
 </script>
